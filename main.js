58e8de00bba9a2fc77e89e7f796a7b5085d9863f
Hyejin Shim <131861332+Jinidudi@users.noreply.github.com>
2023-05-31 17:38:54 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+const card = document.querySelector(".card__inner");
+
+card.addEventListener("click", function (e) {
+  card.classList.toggle('is-flipped');
+});
+// resource:https://github.com/TylerPottsDev/card-flip/blob/master/main.js, https://www.youtube.com/watch?v=lIZ0Vt7FD3Q&t=380s
+
